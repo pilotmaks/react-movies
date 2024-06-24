2097ea2e33ffd6c6e7be256756d1d3b00541a051
pilotmaks <maxim.savine@gmail.com>
2024-06-24 22:25:00 +0500

    version 2 - Movies, Movie

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,7 +1,29 @@
-function Main() {
-    return <main className="container content">
-        Hello from Movies App
-    </main>
+import React from "react"
+import {Movies} from '../components/Movies'
+
+class Main extends React.Component{
+    state = {
+        movies: [],
+    }
+
+    componentDidMount() {
+        fetch('http://www.omdbapi.com/?apikey=ba33e429&s=matrix')
+            .then(respose => respose.json())
+            .then(data => this.setState({movies: data.Search}))
+    }
+
+    render() {
+        const {movies} = this.state;
+        return <main className="container content">
+            {
+                movies.length ? (
+            <Movies movies={this.state.movies}/>
+                ) : <h3>Loading...</h3>
+        }
+        </main>
+
+    }
+
 }
 
 export {Main};
