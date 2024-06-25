004e4d88e48ac890e54579eccb917083e4ffae2c
pilotmaks <maxim.savine@gmail.com>
2024-06-25 23:39:06 +0500

    version 5 - add radiobutton, fetch после обновления state

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,40 +1,39 @@
-import React from "react"
-import { Movies } from '../components/Movies'
-import { Preloader } from "../components/Preloader"
-import { Search } from "../components/Search"
+import React from "react";
+import { Movies } from "../components/Movies";
+import { Preloader } from "../components/Preloader";
+import { Search } from "../components/Search";
 
 class Main extends React.Component {
-    state = {
-        movies: [],
-       
-    }
+  state = {
+    movies: [],
+  };
 
-    componentDidMount() {
-        fetch('http://www.omdbapi.com/?apikey=ba33e429&s=matrix')
-        .then(respose => respose.json())
-        .then(data => this.setState({ movies: data.Search }))
-    }
+  componentDidMount() {
+    fetch("http://www.omdbapi.com/?apikey=ba33e429&s=matrix")
+      .then((respose) => respose.json())
+      .then((data) => this.setState({ movies: data.Search }));
+  }
 
-    searchMovies = (str) => {
-        fetch(`http://www.omdbapi.com/?apikey=ba33e429&s=${str}`)
-        .then(respose => respose.json())
-        .then(data => this.setState({ movies: data.Search }))
-    }
+  searchMovies = (str, type = "all") => {
+    fetch(
+      `http://www.omdbapi.com/?apikey=ba33e429&s=${str}${
+        type !== "all" ? `&type=${type}` : ""
+      }`
+    )
+      .then((respose) => respose.json())
+      .then((data) => this.setState({ movies: data.Search }));
+  };
 
-    render() {
-        const { movies } = this.state;
-      
-        return <main className="container content">
-              <Search searchMovies={this.searchMovies}  />
-            {
-                movies.length ? (
-                    <Movies movies={this.state.movies} />
-                ) : <Preloader />
-            }
-        </main>
+  render() {
+    const { movies } = this.state;
 
-    }
-
+    return (
+      <main className="container content">
+        <Search searchMovies={this.searchMovies} />
+        {movies.length ? <Movies movies={this.state.movies} /> : <Preloader />}
+      </main>
+    );
+  }
 }
 
 export { Main };
