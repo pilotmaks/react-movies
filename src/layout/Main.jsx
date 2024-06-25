bfeb6dfab03997a589eb9dc68900a3b2dfc1969f
pilotmaks <maxim.savine@gmail.com>
2024-06-25 11:52:48 +0500

    version 3 - add Search (only component)

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,7 +1,9 @@
 import React from "react"
-import {Movies} from '../components/Movies'
+import { Movies } from '../components/Movies'
+import { Preloader } from "../components/Preloader"
+import { Search } from "../components/Search"
 
-class Main extends React.Component{
+class Main extends React.Component {
     state = {
         movies: [],
     }
@@ -9,21 +11,23 @@ class Main extends React.Component{
     componentDidMount() {
         fetch('http://www.omdbapi.com/?apikey=ba33e429&s=matrix')
             .then(respose => respose.json())
-            .then(data => this.setState({movies: data.Search}))
+            .then(data => this.setState({ movies: data.Search }))
     }
 
     render() {
-        const {movies} = this.state;
+        const { movies } = this.state;
+      
         return <main className="container content">
+              <Search />
             {
                 movies.length ? (
-            <Movies movies={this.state.movies}/>
-                ) : <h3>Loading...</h3>
-        }
+                    <Movies movies={this.state.movies} />
+                ) : <Preloader />
+            }
         </main>
 
     }
 
 }
 
-export {Main};
+export { Main };
